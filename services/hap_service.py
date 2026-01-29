c91bd9ef77ec89211254e15f1213d4e2c82c3833
Arturo Caballero <arturcabbb@outlook.com>
2026-01-28 21:13:41 -0600

    hacer cambio de direcciones

--- services/hap_service.py
+++ services/hap_service.py
@@ -24,10 +24,10 @@ class HAPService:
         persist_file = str(config.CONFIG_DIR / config.hap.persist_file_name)
         
         self.driver = AccessoryDriver(
-            address='0.0.0.0',
             port= config.hap.port,
             pincode= config.hap.pincode.encode('utf-8'),
-            persist_file=persist_file
+            persist_file=persist_file,
+            listen_address='0.0.0.0'
         )
         
         # Crear bridge
